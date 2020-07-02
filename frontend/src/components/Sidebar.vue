25d5f26b2ad8d5106bfeb5a0d6fdb53144266feb
Kazushi Yamanishi <keironia187149@gmail.com>
2020-07-02 19:06:18 +0900

    create Modal.vue

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -1,6 +1,20 @@
 <template>
   <aside class='sidebar'>
-    <span class="material-icons new-group-btn">add_circle_outline</span>
+    <!-- クリックしたらopenModalメソッドを動かす -->
+    <span @click="openModal" class="material-icons new-group-btn">add_circle_outline</span>
+    <!-- data.modalがtrueになったらModalコンポーネントが表示される -->
+    <Modal @close='closeModal' v-if='modal'>
+      <!-- methodはこっちに書きたいのでslotを使う -->
+      <!-- Modalのslotに差し込む -->
+      <label class='group-form-label'>グループ名</label>
+      <div><input v-model="groupName"></div>
+      <!-- slot ここまで -->
+      <!-- Modalのslot name=footerに差し込む -->
+      <template slot="footer">
+        <button @click="submitGroup">送信</button>
+      </template>
+      <!-- slot name=footer ここまで -->
+    </Modal>
     <section class='group-container'>
       <div class='group'>
         <p class='group-name'>グループ名</p>
@@ -111,6 +125,31 @@
 </template>
 
 <script>
+  import Modal from './Modal.vue'
+
+  export default {
+    components: { Modal },
+    data() {
+      return {
+        modal: false,
+        groupName: ''
+      }
+    },
+    methods: {
+      openModal() {
+        this.modal = true
+      },
+      closeModal() {
+        this.modal = false
+      },
+      submitGroup() {
+        // ここでaction_cable叩く
+        console.log(this.groupName)
+        this.closeModal()
+        this.groupName = ''
+      }
+    }
+  }
 </script>
 
 <style scoped>
@@ -127,6 +166,10 @@
     padding-top: 1.2rem;
     padding-bottom: 1.2rem;
     font-size: 3rem;
+    cursor: pointer;
+  }
+  .group-form-label {
+    color: #222;
   }
   .group-container {
     height: calc(100% - 5.4rem);

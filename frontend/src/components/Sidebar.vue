0bf0e0274bcd0ad6f29d0d7efb7ca7feffc895b3
Kazushi Yamanishi <keironia187149@gmail.com>
2020-07-02 14:13:51 +0900

    markup sidebar

---
+++ frontend/src/components/Sidebar.vue
@@ -0,0 +1,152 @@
+<template>
+  <aside class='Sidebar'>
+    <span class="material-icons NewGroupBtn">add_circle_outline</span>
+    <section class='GroupContainer'>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+      <div class='Group'>
+        <p class='GroupName'>グループ名</p>
+        <span class='GroupNotice'>2</span>
+      </div>
+    </section>
+  </aside>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .Sidebar {
+    height: 100vh;
+    width: 250px;
+    padding: 0 0.7rem;
+    background-color: #ccc;
+    font-size: 1rem;
+    color: white;
+  }
+  .NewGroupBtn {
+    height: 5.4rem;
+    padding-top: 1.2rem;
+    padding-bottom: 1.2rem;
+    font-size: 3rem;
+  }
+  .GroupContainer {
+    height: calc(100% - 5.4rem);
+    overflow: scroll;
+  }
+  .Group {
+    display: flex;
+    margin-bottom: 1.5rem;
+  }
+  .GroupName {
+    margin-right: 1rem;
+  }
+  .GroupNotice {
+    height: 1.5rem;
+    min-width: 2.2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0.75rem;
+    background-color: tomato;
+    font-size: 0.8rem;
+  }
+</style>

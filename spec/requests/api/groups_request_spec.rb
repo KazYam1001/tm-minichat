09eb5abe0fcdf963e6e16a6874f06eb7025c3260
Kazushi Yamanishi <keironia187149@gmail.com>
2020-07-02 21:31:32 +0900

    add api/groups request spec

--- spec/requests/api/groups_request_spec.rb
+++ spec/requests/api/groups_request_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Groups", type: :request do
+  describe '#index (GET /api/groups)' do
+    it '/api/groups を叩くと正常なレスポンスを返す' do
+      # 事前に10件用意する
+      create_list(:group, 10)
 
+      get api_groups_path
+      # 後でデータ件数を数えたいのでresponse.bodyを加工しておく
+      json = JSON.parse(response.body)
+
+      binding.pry
+      expect(response).to have_http_status(:success)
+      # ちゃんと全件取得しているか
+      expect(json.length).to eq(10)
+    end
+  end
 end

e1a618350bb4d1fde1ae6c0333597aa9dc283621
Kazushi Yamanishi <keironia187149@gmail.com>
2020-07-02 20:39:29 +0900

    add package axios and actioncable

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,12 @@
 // Vueの導入と、エントリのインポート
 import Vue from 'vue';
 import App from './App.vue';
+
+// ActionCable周り
+import ActionCable from 'actioncable'
+const cable = ActionCable.createConsumer('ws:localhost:3000/cable');
+Vue.prototype.$cable = cable;
+
 // App.vueをエントリとしてレンダリング
 new Vue({
   el: '#app',

1562ac350527ad3b13143049fdb45af0f2d20d49
Kazushi Yamanishi <keironia187149@gmail.com>
2020-07-07 10:44:03 +0900

    mv store to global

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,11 @@ import ActionCable from 'actioncable'
 const cable = ActionCable.createConsumer('ws:localhost:3000/cable');
 Vue.prototype.$cable = cable;
 
+// storeのグローバル化
+// 全コンポーネントからthis.$storeで呼べる
+import store from './store'
+Vue.prototype.$store = store
+
 // App.vueをエントリとしてレンダリング
 new Vue({
   el: '#app',

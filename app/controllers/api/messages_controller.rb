a4ddd3c525d448e8c40abeb88e2238ef7b489739
Kazushi Yamanishi <keironia187149@gmail.com>
2020-07-07 11:19:01 +0900

    add api/messages#create

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -3,4 +3,16 @@ class Api::MessagesController < ApplicationController
     group = Group.find(params[:group_id])
     render json: group.messages
   end
+
+  def create
+    group = Group.find(params[:group_id])
+    message = group.messages.create(message_params)
+    render json: message
+  end
+
+  private
+
+  def message_params
+    params.permit(:content)
+  end
 end
